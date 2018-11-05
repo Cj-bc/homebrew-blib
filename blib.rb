f21605fb90ae88b1946a731f704a40fd1fcb8c7b
Cj-bc <cj-bc@y-modify.org>
2018-11-05 23:03:21 +0900

    Update: v0.1.2

--- blib.rb
+++ blib.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Blib < Formula
   homepage "https://github.com/Cj-bc/blib"
-  version '0.1.1'
+  version '0.1.2'
   url "https://github.com/Cj-bc/blib/archive/v" + version + ".tar.gz"
   sha256 "e46aaa0c50a364d217fec6ae957a881cef8d677a2c570ae3067fa9d356e3d64e"
 
