f0c35dab1a7a5e8c9cea342a381d6f5a502c42f8
Cj-bc <cj-bc@y-modify.org>
2018-11-05 19:08:06 +0900

    Fix: missing '.'

--- blib.rb
+++ blib.rb
@@ -3,7 +3,7 @@ require "formula"
 class Blib < Formula
   homepage "https://github.com/Cj-bc/blib"
   version '0.1.0'
-  url "https://github.com/Cj-bc/blib/archive/v" + version + "tar.gz"
+  url "https://github.com/Cj-bc/blib/archive/v" + version + ".tar.gz"
   sha256 "d9e4a3a2e92d8daf31862a9bad941147537df0267dc14099b305540cad41fc9c"
 
   def install
