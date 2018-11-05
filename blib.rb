b77426177132620afdd4c37425837ba489589a50
Cj-bc <cj-bc@y-modify.org>
2018-11-05 18:53:05 +0900

    Fix: add =

--- blib.rb
+++ blib.rb
@@ -1,6 +1,6 @@
 class Blib < Formula
   homepage "https://github.com/Cj-bc/blib"
-  version "0.1.0"
+  version = "0.1.0"
   url "https://github.com/Cj-bc/blib/archive/v#{version}.tar.gz"
   sha256 "d9e4a3a2e92d8daf31862a9bad941147537df0267dc14099b305540cad41fc9c"
 
