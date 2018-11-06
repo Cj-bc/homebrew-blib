19ef311bbb0a8d777731ce85aa04ee4b764fc468
Cj-bc <cj-bc@y-modify.org>
2018-11-07 06:24:13 +0900

    Change: use Makefile for install [WIP]

--- blib.rb
+++ blib.rb
@@ -7,10 +7,7 @@ class Blib < Formula
   sha256 "a95e8d7fb951c08a18b7ff0a4a7c95e1fa07dc31ac41c052afb8ac0f48bcdeee"
 
   def install
-    system 'vim +\'%s#\$( cd "\${BASH_SOURCE\[0\]%\/\*}" && pwd )#' + prefix + '#g\' +w! +q blib.oo.sh'
-    system "mv", "blib.oo.sh", "blib"
-    bin.install "blib"
-    prefix.install Dir["deps"], "Makefile", Dir["tests"]
+    system 'make', "root=#{prefix}", "install"
   end
 
   test do
