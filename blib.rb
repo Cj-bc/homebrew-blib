2de14091ccb46028a9feb902b6d707626acf0e4e
Cj-bc <cj-bc@y-modify.org>
2018-11-07 06:22:56 +0900

    Update: v0.2.0

--- blib.rb
+++ blib.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Blib < Formula
   homepage "https://github.com/Cj-bc/blib"
-  version '0.1.3'
+  version '0.2.0'
   url "https://github.com/Cj-bc/blib/archive/v" + version + ".tar.gz"
-  sha256 "da08e0f53d98dbc028c91c72099cf2503737808df9cd23a5bff230b10f5808dd"
+  sha256 "a95e8d7fb951c08a18b7ff0a4a7c95e1fa07dc31ac41c052afb8ac0f48bcdeee"
 
   def install
     system 'vim +\'%s#\$( cd "\${BASH_SOURCE\[0\]%\/\*}" && pwd )#' + prefix + '#g\' +w! +q blib.oo.sh'
