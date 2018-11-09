6e90616a5bb490019fcf0f37d560573d33c85bf4
Cj-bc <cj-bc@y-modify.org>
2018-11-09 11:24:40 +0900

    Update: v0.2.1

--- blib.rb
+++ blib.rb
@@ -2,15 +2,15 @@ require "formula"
 
 class Blib < Formula
   homepage "https://github.com/Cj-bc/blib"
-  version '0.2.0'
+  version '0.2.1'
   url "https://github.com/Cj-bc/blib/archive/v" + version + ".tar.gz"
-  sha256 "a95e8d7fb951c08a18b7ff0a4a7c95e1fa07dc31ac41c052afb8ac0f48bcdeee"
+  sha256 "93612fe110cb2601d181253d2a50e0309c5b45c5ce3491e8c93550f32f53a60e"
 
   def install
     system 'make', "root=#{prefix}", "install"
   end
 
   test do
-    system "make test"
+    system "make root=#{prefix} test"
   end
 end
